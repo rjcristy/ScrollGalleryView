fcf8d255b8c68099eef029c951ff76b9325b01e6
Boris Korogvich <b.korogvich@gmail.com>
2018-01-27 15:01:01 +0300

    added exception class

---
+++ library/src/main/java/com/veinhorn/scrollgalleryview/ScrollGalleryViewException.java
@@ -0,0 +1,15 @@
+package com.veinhorn.scrollgalleryview;
+
+/**
+ * Created by veinhorn on 1/27/18.
+ */
+
+public class ScrollGalleryViewException extends Exception {
+    public ScrollGalleryViewException() {
+
+    }
+
+    public ScrollGalleryViewException(String message) {
+        super(message);
+    }
+}
